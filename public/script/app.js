4cc709e117a97b4a054a904d287fb4acc95f89be
cclivick <46388110+cclivick@users.noreply.github.com>
2019-08-19 21:00:01 -0500

    Style changes to article cards, modal, and saved article page - also added media queries

--- public/script/app.js
+++ public/script/app.js
@@ -4,7 +4,7 @@ console.log("Loaded")
 $.getJSON("/articles", function(data) {
     for( var i = 0 ; i < data.length ; i++ ) {
         if(data[i].saved === false) {
-            $("#articles").append('<div class="row"><div class="card mb-3 artCard" style="max-width: 90%; min-width: 90%;"><div class="row no-gutters"><div class="col-md-4"><img src="' + data[i].image + '" class="card-img artImg" alt="..."></div><div class="col-md-8"><div class="card-body"><h5 class="card-title artTitle">' + data[i].title + '</h5><a class="artLink" href="' + data[i].link + '">Link to Metal Injection Article</a><br><br><a href="#" class="btn btn-primary savButton" data-id="' + data[i]._id + '">Save Article</a></div></div></div></div>');
+            $("#articles").append('<div class="row"><div class="card mb-3 artCard" style="max-width: 100%; min-width: 100%;"><div class="row no-gutters"><div class="col-md-4 picCol"><img src="' + data[i].image + '" class="card-img artImg" alt="..."></div><div class="col-md-8"><div class="card-body"><h5 class="card-title artTitle">' + data[i].title + '</h5><a class="artLink" href="' + data[i].link + '">Link to Metal Injection Article</a><br><br><a href="#" class="btn btn-success savButton" data-id="' + data[i]._id + '">Save Article</a></div></div></div></div>');
         }
     }
 });
@@ -13,7 +13,7 @@ $.getJSON("/articles", function(data) {
 $.getJSON("/articles", function(data) {
     for( var j = 0 ; j < data.length ; j++ ) {
         if(data[j].saved === true) {
-            $("#savedArticles").append('<div class="row"><div class="card mb-3 artCard" style="max-width: 90%; min-width: 90%;"><div class="row no-gutters"><div class="col-md-4"><img src="' + data[j].image + '" class="card-img artImg" alt="..."></div><div class="col-md-8"><div class="card-body"><h5 class="card-title artTitle">' + data[j].title + '</h5><a class="artLink" href="' + data[j].link + '">Link to Metal Injection Article</a><br><br><button class="btn btn-primary unSaveButton" data-id="' + data[j]._id + '">Unsave</button><button data-toggle="modal" data-target="#noteModal" class="btn btn-primary notesButton" data-id="' + data[j]._id + '">See/Add Notes</button></div></div></div></div>');
+            $("#savedArticles").append('<div class="row"><div class="card mb-3 artCard" style="max-width: 100%; min-width: 100%;"><div class="row no-gutters"><div class="col-md-4 picCol"><img src="' + data[j].image + '" class="card-img artImg" alt="..."></div><div class="col-md-8"><div class="card-body"><h5 class="card-title artTitle">' + data[j].title + '</h5><a class="artLink" href="' + data[j].link + '">Link to Metal Injection Article</a><br><br><button class="btn btn-danger unSaveButton" data-id="' + data[j]._id + '">Unsave</button><button data-toggle="modal" data-target="#noteModal" class="btn btn-primary notesButton" data-id="' + data[j]._id + '">See/Add Notes</button></div></div></div></div>');
         }
     }
 })
